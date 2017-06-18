c1c1c32e5eab9c80925a33fc79f66d56f3c998d4
wanglianghai <1540446221@qq.com>
2017-06-18 12:59:03 +0800

    class control and show separate, set adapter data need notifyDataSetChanged();

--- app/src/main/java/com/bignerdranch/android/rxjavasampelswlh/module/MapFragment.java
+++ app/src/main/java/com/bignerdranch/android/rxjavasampelswlh/module/MapFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bignerdranch.android.rxjavasampelswlh.BaseFragment;
@@ -17,6 +19,7 @@ import com.bignerdranch.android.rxjavasampelswlh.model.GankBeautyResult;
 import com.bignerdranch.android.rxjavasampelswlh.model.Item;
 import com.bignerdranch.android.rxjavasampelswlh.network.Network;
 import com.bignerdranch.android.rxjavasampelswlh.util.GankBeautyResultMap;
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -34,7 +37,9 @@ import rx.schedulers.Schedulers;
 
 public class MapFragment extends BaseFragment {
     private static final String TAG = "MapFragment";
-    private int mPages = 1;
+    private int mPages = 2;
+
+    private MapAdapter mAdapter = new MapAdapter();
     @Bind(R.id.map_page) TextView mPageTextView;
     @Bind(R.id.button_previous) Button mPreviousButton;
     @Bind(R.id.fragment_map_recycler_view) RecyclerView mRecyclerView;
@@ -53,7 +58,8 @@ public class MapFragment extends BaseFragment {
 
         @Override
         public void onNext(List<Item> gankBeautyResults) {
-            Log.i(TAG, "onNext: json = " + gankBeautyResults.size());
+            mRefreshLayout.setRefreshing(false);
+            mAdapter.setItems(gankBeautyResults);
         }
     };
 
@@ -69,23 +75,15 @@ public class MapFragment extends BaseFragment {
         updateButton();
     }
 
-    private void loadPage() {
-        unSubscription();
-        mSubscription = Network.getGankApi()
-                .getBeauty(10, mPages)
-                .map(GankBeautyResultMap.getInstance())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(mObserver);
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         ButterKnife.bind(this, view);
+        mRefreshLayout.setEnabled(false);
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+        mRecyclerView.setAdapter(mAdapter);
         updateButton();
-        Log.i(TAG, "onCreateView: Map");
         return view;
     }
 
@@ -100,6 +98,8 @@ public class MapFragment extends BaseFragment {
     }
 
     void updateButton() {
+        mAdapter.setItems(null);
+        mRefreshLayout.setRefreshing(true);
         mPageTextView.setText(getString(R.string.page_number, mPages + ""));
         if (mPages == 1) {
             mPreviousButton.setEnabled(false);
@@ -107,5 +107,56 @@ public class MapFragment extends BaseFragment {
             mPreviousButton.setEnabled(true);
         }
         loadPage();
+    }
+
+    private void loadPage() {
+        unSubscription();
+        Log.i(TAG, "loadPage: " + mPages);
+        mSubscription = Network.getGankApi()
+                .getBeauty(10, mPages)
+                .map(GankBeautyResultMap.getInstance())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(mObserver);
+    }
+
+    class MapHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.map_item_image) ImageView mImageView;
+        @Bind(R.id.map_item_text) TextView mTextView;
+
+        public MapHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bindItem(Item item) {
+            Glide.with(getActivity()).load(item.mUrl).into(mImageView);
+            mTextView.setText(item.mDate);
+        }
+    }
+
+    private class MapAdapter extends RecyclerView.Adapter<MapHolder> {
+        private List<Item> mItems;
+
+        @Override
+        public MapHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = getActivity().getLayoutInflater().inflate(R.layout.map_item, parent, false);
+            return new MapHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MapHolder holder, int position) {
+            holder.bindItem(mItems.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems == null ? 0 : mItems.size();
+        }
+
+        public void setItems(List<Item> items) {
+            mItems = items;
+            notifyDataSetChanged();
+        }
     }
 }
